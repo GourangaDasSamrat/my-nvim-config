3733bf5242f816ca53e54ad956d8b0ff3860269e
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2025-10-29 13:13:54 +0600

    add word wrap feature and dracula theme

---
+++ lua/plugins/dracula.lua
@@ -0,0 +1,12 @@
+return {
+  -- Add the dracula plugin
+  { "Mofiqul/dracula.nvim" },
+
+  -- Configure LazyVim to load dracula
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
